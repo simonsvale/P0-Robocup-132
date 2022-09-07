d7341156ce1eacaaf9cae55db6885236fa58abfa
simonsvale <s.risbjerg@stofanet.dk>
2022-09-07 12:11:25 +0200

    Co-authored-by: Mikkel-coder <Mikkel-coder@users.noreply.github.com> Co-authored-by: jwcmckay@gmail.com <jwcmckay@gmail.com> Co-authored-by: dahl00095 <dahl00095@users.noreply.github.com> Co-authored-by: Andersen2407 <Andersen2407@users.noreply.github.com>

--- main.py
+++ main.py
@@ -16,18 +16,14 @@ motorR = Motor(Port.A, Direction.CLOCKWISE)
 # Assigning the small motor to variable.
 motorGrip = Motor(Port.C, Direction.CLOCKWISE)
 
-
 # Assigning the colour sensor to a variable.
 linie_sensor = ColorSensor(Port.S4)
 
 # Assigning the distance (Ultrasonic sensor) sensor to a variable.
-distance_sensor = UltrasonicSensor(Port.S3)
+dist_sensor = UltrasonicSensor(Port.S3)
 
 #Wheel motors drivebase
 robot1 = DriveBase(right_motor = motorL, left_motor = motorR, wheel_diameter=22, axle_track=10)
-
-#Claw drivebase
-claw = DriveBase(motorGrip = motor, wheel_diameter=22, axle_track=10)
 
 # Reflection variables calculated 
 BLACK = 60
@@ -40,28 +36,48 @@ DRIVE_SPEED = 1000
 # Turning gain variable, higher variable = more turn
 PROPORTIONAL_GAIN = 4
 
-while True:
-    # Calculate the deviation from the threshold.
-    deviation = linie_sensor.reflection() - threshold
-
-    kontroller.screen.draw_text(30, 20, linie_sensor.reflection())
-    
-    # Calculate the turn rate.
-    turn_rate = PROPORTIONAL_GAIN * deviation
-
-    # Set the drive base speed and turn rate.
-    robot1.drive(DRIVE_SPEED, turn_rate)
-
-    # stops at black border.
-    if  8 < linie_sensor.reflection() < 11:
-         robot1.motorR.brake()
-         
-    wait(10)
-    kontroller.screen.clear()
-
 #Script for gripping the flask at a certain distance
-if distance < 3.0:
+if dist_sensor.distance() < 400:
+
      #Runs the claw for (speed,time)
-     robot1.motorGrip.run_time(100, 10)
+     kontroller.speaker.beep()
+
+
+
+# Calculate the deviation from the threshold.
+deviation = linie_sensor.reflection() - threshold
+
+kontroller.screen.draw_text(30, 20, linie_sensor.reflection())
+    
+# Calculate the turn rate.
+turn_rate = PROPORTIONAL_GAIN * deviation
+
+# Set the drive base speed and turn rate.
+robot1.drive(DRIVE_SPEED, turn_rate)
+
+
+
+class linie_follow:
+      if  8 < linie_sensor.reflection() < 11:
+          #robot1.motorR.brake()
+          kontroller.speaker.beep()
+
+#wait(1000)
+#kontroller.screen.clear()
+
+
+
+
+         
+     
+
+
+    
+
+
+
+
+    
+
 
 
