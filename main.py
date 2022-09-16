e43027fe54dae2f1341d69c2fb56a9c841f60f39
simonsvale <s.risbjerg@stofanet.dk>
2022-09-16 09:17:13 +0200

    Added a calibrate function, to calibrate the sensor before each attempt at the course.

--- main.py
+++ main.py
@@ -21,21 +21,23 @@ kontroller = EV3Brick()                           # Main LEGO brick
 
 motorGrip = Motor(Port.C, Direction.CLOCKWISE)    # Assigning the small motor to a variable.
 
-motorL = Motor(Port.D, Direction.CLOCKWISE)       # Assigning the motors to variables.
-motorR = Motor(Port.A, Direction.CLOCKWISE)
+motorL = Motor(Port.A, Direction.CLOCKWISE)       # Assigning the motors to variables.
+motorR = Motor(Port.D, Direction.CLOCKWISE)
 
 dist_sensor = UltrasonicSensor(Port.S3)           # Assigning the distance (Ultrasonic sensor) sensor to a variable.
-linie_sensor = ColorSensor(Port.S4)               # Assigning the colour sensor to a variable.
+line_sensor = ColorSensor(Port.S4)               # Assigning the colour sensor to a variable.
 
-wheels_drive = DriveBase(right_motor = motorL, left_motor = motorR, wheel_diameter=50, axle_track=10)          # Wheel motors drivebase
+wheels_drive = DriveBase(right_motor = motorR, left_motor = motorL, wheel_diameter=50, axle_track=10)         # Wheel motors drivebase
 
-BLACK = 67 #grey
-WHITE = 99
+calibration_drive = DriveBase(right_motor = motorR, left_motor = motorL, wheel_diameter=50, axle_track=10)  
+
+BLACK = 40 #grey
+WHITE = 55
 threshold = (BLACK + WHITE) / 2                    # Reflection variables calculated 
 
-drive_speed = 100                                  # Used power of motor in % ?
+drive_speed = 180                              # Used power of motor in % ?
 
-turn_gain = 4                                      # Turning gain variable, higher variable = more turn
+turn_gain = 8                                # Turning gain variable, higher variable = more turn
 
 section_number = 0
 
@@ -44,57 +46,45 @@ section_number = 0
 # FUNCTIONS
 #
 
-
-# repeat forever
-def forever(): 
-     global section_number 
+# Calibrates the robot's color sensor, to the grey line, and the white course.
+def calibrate():
+     # Calibrate 
+     calibrated_grey_line = line_sensor.reflection()
+     kontroller.screen.draw_text(1, 1, line_sensor.reflection()) # debug
      
-     line_follow()
+     # Turn the robot.
+     motorL = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+     calibration_drive.drive(-100, 100)
+     wait(400)
+     calibration_drive.stop()
      
-     if section_number == 1:
-          section1()
-          
-     if section_number == 2:
-          section2()
-          
-     if section_number == 3:
-          section3()
-          kontroller.speaker.say("I completed the course")
+     # Drive forward, calibrate and stop.
+     wait(50)
+     motorL = Motor(Port.D, Direction.CLOCKWISE)
+     calibration_drive.drive(100, 100)
+     wait(1000)
+     calibration_drive.stop()
+     calibrated_white_line = line_sensor.reflection()
+     kontroller.screen.draw_text(1, 20, line_sensor.reflection()) # debug
      
-  
-
-
-          
-     kontroller.speaker.say("section 0 done")
-          
-
-def section1():
-     # Doing something static 
-     while section_number == 1:
-          debug_screen_draw_reflection()
-          section1_drive()
-          black_line_stop()
-     
-     kontroller.speaker.say("section 1 done")
-
-
-def section2():
-     1+1
-     
-
-def section3():
-     1+2
-
-def section1_drive():
-     print('doing some driving :-)')
+     # Drive backwards and turn again.
      wait(100)
-     print('doing some more of that :-)')
+     calibration_drive.drive(-100, 100)
+     wait(1000)
+     calibration_drive.stop()
+     wait(50)
+     motorL = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+     calibration_drive.drive(100, 100)
+     wait(400)
+     calibration_drive.stop()
+     motorL = Motor(Port.D, Direction.CLOCKWISE)
+
 
 
 
 def line_follow():
      # Calculate the deviation from the threshold.
-     deviation = linie_sensor.reflection() - threshold
+     deviation = line_sensor.reflection() - threshold
  
      # Calculate the turn rate.
      turn_rate = turn_gain * deviation
@@ -102,13 +92,13 @@ def line_follow():
      # Set the drive base speed and turn rate.
      wheels_drive.drive(drive_speed, turn_rate)
      
-     black_line_stop()
+     #black_line_stop()
      
 
 def black_line_stop():
      global section_number
-     if  8 < linie_sensor.reflection() < 11:
-          #robot1.motorR.brake()
+     if  3 < line_sensor.reflection() < 10:
+          wheels_drive.motorR.brake()
           section_number += 1
           print('stopped at a black line on section', section_number)
           
@@ -122,16 +112,16 @@ def grip_flask():
 
 
 
-#
-# debug
-#
+"""
+debug
+"""
 
 def debug_screen_draw_reflection():
-     kontroller.screen.draw_text(1, 1, linie_sensor.reflection()) 
-     wait(100) 
+     kontroller.screen.draw_text(1, 1, line_sensor.reflection()) 
+     wait(50) 
      kontroller.screen.clear()  
 
 
-forever()
-
+calibrate()
+line_follow()
 #END OF FILE
